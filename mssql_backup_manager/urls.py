36d3ee58cb261f8aa2047e8163706f08259fc540
Jurgens Steyn <40264293+Jurgens92@users.noreply.github.com>
2025-08-21 11:53:08 +0200

    Update

--- mssql_backup_manager/urls.py
+++ mssql_backup_manager/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('dashboard')),  # Redirect root to dashboard
-    path('', include('backup_app.urls')),
+    path('', include('backup_app.urls')),  # Remove the redirect, just include backup_app URLs
 ]
